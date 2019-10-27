5ab2dd5a0649c855b4a1a1533d61834d39c24162
Filip Brsjak <brsjakproductions@gmail.com>
2019-10-27 13:28:25 +0100

    Add files via upload

---
+++ src/components/MainContent.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function MainContent(){
+    return(
+        <div>
+            <p>
+                <input type="checkbox" />
+                Place holder for to do item 1
+            </p>
+            <p>
+                <input type="checkbox" />
+                Place holder for to do item 2
+            </p>
+            <p>
+                <input type="checkbox" />
+                Place holder for to do item 3
+            </p>
+            <p>
+                <input type="checkbox" />
+                Place holder for to do item 4
+            </p>
+        </div>
+    )
+}
+
+export default MainContent
